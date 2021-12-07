ff27260b5facd9df1463d04673b808840f7e3a31
ClarkUPS <90676910+ClarkUPS@users.noreply.github.com>
2021-12-06 19:17:23 -0800

    added the rest of the ball models, added better resolution background. Can't figure out how to randomise the background useing the .choose function.

--- game.js
+++ game.js
@@ -1,150 +1,241 @@
 /**
- * @class Game 
+ * @class Game
  * @author Tristan Gaeta
- * 
+ *
  * @description Instances of a Game object contain a matter.js physics engine and renderer, which are run when
- * the game object is created. All of the items in a game's engine-world are created through static methods 
+ * the game object is created. All of the items in a game's engine-world are created through static methods
  * within the GameObjects class.
  */
 class Game {
+  /**
+   * @constructor creates new instances of a Game object. The display canvas for
+   * this game object is appended to the current document-body and both the engine
+   * and renderer are run on creation.
+   */
+  constructor() {
+    this.engine = Matter.Engine.create({
+      enableSleeping: true,
+      velocityIterations: 10,
+      positionIterations: 10,
+    });
 
-    /**
-     * @constructor creates new instances of a Game object. The display canvas for
-     * this game object is appended to the current document-body and both the engine 
-     * and renderer are run on creation.
-     */
-    constructor() {
-        this.engine = Matter.Engine.create({ enableSleeping: true, velocityIterations: 10, positionIterations: 10 });
+    this.renderer = this.createRenderer();
 
-        this.renderer = this.createRenderer();
+    this.generator = new Generator();
 
-        this.generator = new Generator();
+    this.mouseConstraint = GameObjects.mouseConstraint(this);
 
-        this.mouseConstraint = GameObjects.mouseConstraint(this);
+    this.slingShot = GameObjects.slingShot(
+      this,
+      (Generator.WIDTH_RATIO * Generator.WORLD_SCALE) / 8,
+      (Generator.HEIGHT_RATIO * 3 * Generator.WORLD_SCALE) / 4
+    );
 
-        this.slingShot = GameObjects.slingShot(this, Generator.WIDTH_RATIO * Generator.WORLD_SCALE / 8, Generator.HEIGHT_RATIO * 3 * Generator.WORLD_SCALE / 4);
+    this.leftWall = Matter.Bodies.rectangle(
+      -2 * GameObjects.BLOCK_SIZE,
+      (Generator.HEIGHT_RATIO * Generator.WORLD_SCALE) / 2,
+      4 * GameObjects.BLOCK_SIZE,
+      2 * Generator.HEIGHT_RATIO * Generator.WORLD_SCALE,
+      {
+        isStatic: true,
+        label: "Wall",
+        friction: 1,
+        render: { visible: false },
+        collisionFilter: { group: 1, category: 0b0010, mask: 4294967293 },
+      }
+    );
 
-        this.leftWall = Matter.Bodies.rectangle(-2 * GameObjects.BLOCK_SIZE, Generator.HEIGHT_RATIO * Generator.WORLD_SCALE / 2, 4 * GameObjects.BLOCK_SIZE, 2 * Generator.HEIGHT_RATIO * Generator.WORLD_SCALE,
-            { isStatic: true, label: "Wall", friction: 1, render: { visible: false }, collisionFilter: { 'group': 1, 'category': 0b0010, 'mask': 4294967293 } });
+    this.rightWall = Matter.Bodies.rectangle(
+      Generator.WIDTH_RATIO * Generator.WORLD_SCALE +
+        2 * GameObjects.BLOCK_SIZE,
+      (Generator.HEIGHT_RATIO * Generator.WORLD_SCALE) / 2,
+      4 * GameObjects.BLOCK_SIZE,
+      2 * Generator.HEIGHT_RATIO * Generator.WORLD_SCALE,
+      {
+        isStatic: true,
+        label: "Wall",
+        friction: 1,
+        render: { visible: false },
+        collisionFilter: { group: 1, category: 0b0010, mask: 4294967293 },
+      }
+    );
 
-        this.rightWall = Matter.Bodies.rectangle(Generator.WIDTH_RATIO * Generator.WORLD_SCALE + 2 * GameObjects.BLOCK_SIZE, Generator.HEIGHT_RATIO * Generator.WORLD_SCALE / 2, 4 * GameObjects.BLOCK_SIZE, 2 * Generator.HEIGHT_RATIO * Generator.WORLD_SCALE,
-            { isStatic: true, label: "Wall", friction: 1, render: { visible: false }, collisionFilter: { 'group': 1, 'category': 0b0010, 'mask': 4294967293 } });
+    this.ground = Matter.Bodies.rectangle(
+      (Generator.WIDTH_RATIO * Generator.WORLD_SCALE) / 2,
+      Generator.FLOOR_HEIGHT + 2 * GameObjects.BLOCK_SIZE,
+      Generator.WIDTH_RATIO * Generator.WORLD_SCALE,
+      4 * GameObjects.BLOCK_SIZE,
+      {
+        isStatic: true,
+        label: "Ground",
+        friction: 1,
+        render: {
+          sprite: {
+            texture: "images/Backgrounds/Grass_Long.png",
+            xScale: GameObjects.BLOCK_SIZE / 32,
+            yScale: GameObjects.BLOCK_SIZE / 32,
+          },
+        },
+      }
+    );
 
-        this.ground = Matter.Bodies.rectangle(Generator.WIDTH_RATIO * Generator.WORLD_SCALE / 2, Generator.FLOOR_HEIGHT + 2 * GameObjects.BLOCK_SIZE, Generator.WIDTH_RATIO * Generator.WORLD_SCALE, 4 * GameObjects.BLOCK_SIZE,
-            { isStatic: true, label: "Ground", friction: 1, render: { sprite: { texture: "images/Grass_Long.png", xScale: GameObjects.BLOCK_SIZE / 32, yScale: GameObjects.BLOCK_SIZE / 32 } } });
+    this.streaks = 0;
 
-        this.streaks = 0;
+    this.difficulty = 0;
 
-        this.difficulty = 0;
+    //We save the previous velocity for every body within the game-world,
+    //and remove all projectiles on sleep.
+    Matter.Events.on(this.engine, "beforeUpdate", (event) => {
+      let bodies = Matter.Composite.allBodies(event.source.world);
+      for (let body of bodies) {
+        for (let part of body.parts) {
+          if (part.label == "Projectile" && part.isSleeping) {
+            new Audio(
+              `sounds/sfx-pop${Matter.Common.choose(["", 3, 4, 5, 6])}.mp3`
+            ).play();
+            Matter.Composite.remove(this.engine.world, part, true);
+          } else {
+            part.velocityPrev = part.velocity;
+          }
+        }
+      }
+    });
 
+    Matter.Events.on(this.engine, "afterUpdate", (event) => {
+      let bodies = Matter.Composite.allBodies(event.source.world);
+      let levelOver = true;
+      for (let body of bodies) {
+        if (body.parts.length > 1) {
+          for (let part of body.parts) {
+            part.angle = body.angle;
+          }
+        }
+        if (body.label == "Block") {
+          levelOver = false;
+          if (body.shockAbsorbed > body.hp) {
+            new Audio(
+              `sounds/sfx-pop${Matter.Common.choose(["", 3, 4, 5, 6])}.mp3`
+            ).play();
+            Matter.Composite.remove(event.source.world, body, true);
+          }
+        } else if (body.label == "Projectile") {
+          levelOver = false;
+          if (
+            body.bounds.min.y >
+              Generator.HEIGHT_RATIO * Generator.WORLD_SCALE ||
+            body.bounds.min.x > Generator.WIDTH_RATIO * Generator.WORLD_SCALE ||
+            body.bounds.max.x < 0
+          ) {
+            Matter.Composite.remove(event.source.world, body, true);
+          }
+        }
+      }
 
-        //We save the previous velocity for every body within the game-world,
-        //and remove all projectiles on sleep.
-        Matter.Events.on(this.engine, 'beforeUpdate', (event) => {
-            let bodies = Matter.Composite.allBodies(event.source.world)
-            for (let body of bodies) {
-                for (let part of body.parts) {
-                    if (part.label == "Projectile" && part.isSleeping) {
-                        new Audio(`sounds/sfx-pop${Matter.Common.choose(["", 3, 4, 5, 6])}.mp3`).play()
-                        Matter.Composite.remove(this.engine.world, part, true)
-                    } else {
-                        part.velocityPrev = part.velocity;
-                    }
-                }
-            }
-        })
+      if (levelOver) {
+        Matter.Composite.remove(
+          this.engine.world,
+          this.engine.world.composites,
+          true
+        );
+        Matter.Composite.add(this.engine.world, this.generator.nextLevel());
+      }
+    });
 
-        Matter.Events.on(this.engine, "afterUpdate", (event) => {
-            let bodies = Matter.Composite.allBodies(event.source.world)
-            let levelOver = true;
-            for (let body of bodies) {
-                if (body.parts.length > 1) {
-                    for (let part of body.parts) {
-                        part.angle = body.angle;
-                    }
-                }
-                if (body.label == "Block") {
-                    levelOver = false;
-                    if (body.shockAbsorbed > body.hp) {
-                        new Audio(`sounds/sfx-pop${Matter.Common.choose(["", 3, 4, 5, 6])}.mp3`).play()
-                        Matter.Composite.remove(event.source.world, body, true)
-                    }
-                } else if (body.label == "Projectile") {
-                    levelOver = false;
-                    if (body.bounds.min.y > Generator.HEIGHT_RATIO * Generator.WORLD_SCALE || body.bounds.min.x > Generator.WIDTH_RATIO * Generator.WORLD_SCALE || body.bounds.max.x < 0) {
-                        Matter.Composite.remove(event.source.world, body, true)
-                    }
-                }
-            }
-
-            if (levelOver) {
-                Matter.Composite.remove(this.engine.world, this.engine.world.composites, true);
-                Matter.Composite.add(this.engine.world, this.generator.nextLevel())
-
-            }
-        })
-
-        //We update the 'shockAbsorbed' by each body on impact. This value is calculated 
-        //pair-wise by the difference in linear momentum of the two objects before impact.
-        Matter.Events.on(this.engine, "collisionStart", (event) => {
-            for (let pair of event.pairs) {
-                let momentumA = Matter.Vector.mult(pair.bodyA.parent.velocityPrev, pair.bodyA.isStatic ? 0 : pair.bodyA.mass);
-                let momentumB = Matter.Vector.mult(pair.bodyB.parent.velocityPrev, pair.bodyB.isStatic ? 0 : pair.bodyB.mass);
-                let mag = Matter.Vector.magnitude(Matter.Vector.sub(momentumA, momentumB));
-                if (pair.bodyA.parent.label != "Projectile" && pair.bodyB.parent.label != "Projectile") {
-                    if (pair.bodyA.label == "Ground" || pair.bodyB.parent.label == "Ground") {
-                        mag *= 15;
-                    }
-                }
-
-                pair.bodyA.shockAbsorbed = pair.bodyA.shockAbsorbed || 0;
-                pair.bodyA.shockAbsorbed += Math.floor(mag);
-                pair.bodyA.parent.shockAbsorbed = pair.bodyA.parent.shockAbsorbed || 0;
-                pair.bodyA.parent.shockAbsorbed += pair.bodyA.shockAbsorbed;
-
-                pair.bodyB.shockAbsorbed = pair.bodyB.shockAbsorbed || 0;
-                pair.bodyB.shockAbsorbed += Math.floor(mag);
-                pair.bodyB.parent.shockAbsorbed = pair.bodyB.parent.shockAbsorbed || 0;
-                pair.bodyB.parent.shockAbsorbed += pair.bodyB.shockAbsorbed;
-            }
-        })
-
-
-        Matter.Composite.add(this.engine.world, [this.generator.nextLevel(), this.ground, this.leftWall, this.rightWall, this.mouseConstraint, this.slingShot])
-        Matter.Render.run(this.renderer);
-        Matter.Runner.run(this.engine);
-    }
-
-    /**
-     * @description this method creates a renderer object and maximizes the canvas
-     * size.
-     * 
-     * @returns a matter.js render object
-     */
-    createRenderer() {
-        let pageWidth = window.innerWidth - 16;
-        let pageHeight = window.innerHeight - 16;
-        if (pageWidth / pageHeight > Generator.WIDTH_RATIO / Generator.HEIGHT_RATIO) {
-            pageWidth = Generator.WIDTH_RATIO * pageHeight / Generator.HEIGHT_RATIO
-        } else {
-            pageHeight = Generator.HEIGHT_RATIO * pageWidth / Generator.WIDTH_RATIO
+    //We update the 'shockAbsorbed' by each body on impact. This value is calculated
+    //pair-wise by the difference in linear momentum of the two objects before impact.
+    Matter.Events.on(this.engine, "collisionStart", (event) => {
+      for (let pair of event.pairs) {
+        let momentumA = Matter.Vector.mult(
+          pair.bodyA.parent.velocityPrev,
+          pair.bodyA.isStatic ? 0 : pair.bodyA.mass
+        );
+        let momentumB = Matter.Vector.mult(
+          pair.bodyB.parent.velocityPrev,
+          pair.bodyB.isStatic ? 0 : pair.bodyB.mass
+        );
+        let mag = Matter.Vector.magnitude(
+          Matter.Vector.sub(momentumA, momentumB)
+        );
+        if (
+          pair.bodyA.parent.label != "Projectile" &&
+          pair.bodyB.parent.label != "Projectile"
+        ) {
+          if (
+            pair.bodyA.label == "Ground" ||
+            pair.bodyB.parent.label == "Ground"
+          ) {
+            mag *= 15;
+          }
         }
 
-        let render = Matter.Render.create({
-            element: document.body,
-            engine: this.engine,
-            bounds: Matter.Bounds.create([{ x: 0, y: 0 }, { x: Generator.WIDTH_RATIO * Generator.RENDER_SCALE, y: 0 }, { x: Generator.WIDTH_RATIO * Generator.RENDER_SCALE, y: Generator.HEIGHT_RATIO * Generator.RENDER_SCALE }, { x: 0, y: Generator.HEIGHT_RATIO * Generator.RENDER_SCALE }]),
-            hasBounds: true,
-            options: {
-                background: "images/background.png",
-                //showDebug: true,
-                showSleeping: false,
-                width: pageWidth,
-                height: pageHeight,
-                wireframes: false,
-                wireframeBackground: false,
-            },
-        });
-        return render;
+        pair.bodyA.shockAbsorbed = pair.bodyA.shockAbsorbed || 0;
+        pair.bodyA.shockAbsorbed += Math.floor(mag);
+        pair.bodyA.parent.shockAbsorbed = pair.bodyA.parent.shockAbsorbed || 0;
+        pair.bodyA.parent.shockAbsorbed += pair.bodyA.shockAbsorbed;
+
+        pair.bodyB.shockAbsorbed = pair.bodyB.shockAbsorbed || 0;
+        pair.bodyB.shockAbsorbed += Math.floor(mag);
+        pair.bodyB.parent.shockAbsorbed = pair.bodyB.parent.shockAbsorbed || 0;
+        pair.bodyB.parent.shockAbsorbed += pair.bodyB.shockAbsorbed;
+      }
+    });
+
+    Matter.Composite.add(this.engine.world, [
+      this.generator.nextLevel(),
+      this.ground,
+      this.leftWall,
+      this.rightWall,
+      this.mouseConstraint,
+      this.slingShot,
+    ]);
+    Matter.Render.run(this.renderer);
+    Matter.Runner.run(this.engine);
+  }
+
+  /**
+   * @description this method creates a renderer object and maximizes the canvas
+   * size.
+   *
+   * @returns a matter.js render object
+   */
+  createRenderer() {
+    let pageWidth = window.innerWidth * 0.75 - 16;
+    let pageHeight = window.innerHeight - 16;
+    if (
+      pageWidth / pageHeight >
+      Generator.WIDTH_RATIO / Generator.HEIGHT_RATIO
+    ) {
+      pageWidth = (Generator.WIDTH_RATIO * pageHeight) / Generator.HEIGHT_RATIO;
+    } else {
+      pageHeight = (Generator.HEIGHT_RATIO * pageWidth) / Generator.WIDTH_RATIO;
     }
+
+    let render = Matter.Render.create({
+      element: document.body, //selector and append to div (id).
+      engine: this.engine,
+      bounds: Matter.Bounds.create([
+        { x: 0, y: 0 },
+        { x: Generator.WIDTH_RATIO * Generator.RENDER_SCALE, y: 0 },
+        {
+          x: Generator.WIDTH_RATIO * Generator.RENDER_SCALE,
+          y: Generator.HEIGHT_RATIO * Generator.RENDER_SCALE,
+        },
+        { x: 0, y: Generator.HEIGHT_RATIO * Generator.RENDER_SCALE },
+      ]),
+      hasBounds: true,
+      options: {
+        background: Matter.Common.choose([
+          "images/Backgrounds/Cave-Background.png",
+          "images/Backgrounds/Forest-Background.png",
+        ]),
+        //showDebug: true,
+        showSleeping: false,
+        width: pageWidth,
+        height: pageHeight,
+        wireframes: false,
+        wireframeBackground: false,
+      },
+    });
+    return render;
+  }
 }
--- gameObjects.js
+++ gameObjects.js
@@ -1,151 +1,220 @@
 /***
  * @class GameObjects
  * @author Tristan Gaeta
- * 
+ *
  * @description The GameObjects class contains static methods for creating game objects.
- * 
+ *
  */
 class GameObjects {
-    static _PRE_SHOT_COLLISION_FILTER = { 'group': 1, 'category': 0b0010, 'mask': 0b0000 }
+  static _PRE_SHOT_COLLISION_FILTER = {
+    group: 1,
+    category: 0b0010,
+    mask: 0b0000,
+  };
 
-    static _POST_SHOT_COLLISION_FILTER = { 'group': 0, 'category': 0b0010, 'mask': -1 }
+  static _POST_SHOT_COLLISION_FILTER = { group: 0, category: 0b0010, mask: -1 };
 
-    static BLOCK_SIZE = 64;
+  static BLOCK_SIZE = 64;
 
-    static SLING_SHOT_LENGTH = 4 * GameObjects.BLOCK_SIZE;
+  static SLING_SHOT_LENGTH = 4 * GameObjects.BLOCK_SIZE;
 
-    static projectile(x, y) {
-        let txt = Matter.Common.choose(["images/balls/Color Ball.png", "images/balls/Gradient-ball.png"])
-        let body = Matter.Bodies.circle(x, y, GameObjects.BLOCK_SIZE / 2, {
-            render: { sprite: { texture: txt, xScale: GameObjects.BLOCK_SIZE / 64, yScale: GameObjects.BLOCK_SIZE / 64 } },
-            restitution: 0.8,
-            collisionFilter: GameObjects._PRE_SHOT_COLLISION_FILTER,
-            label: "Projectile",
-            powerActivated: false,
-            launched: false,
-            activatePower: undefined,
-            frictionAir: 0.005,
-            sleepThreshold: 1500 / 16.666,
-            density: 0.007
-        });
+  static projectile(x, y) {
+    let txt = Matter.Common.choose([
+      "images/balls/Color Ball.png",
+      "images/balls/Gradient-ball.png",
+      "images/balls/8-Ball.png",
+      "images/balls/Banana-Ball.png",
+      "images/balls/Death-Ball.png",
+      "images/balls/Red-Ball.png",
+      "images/balls/Star-Ball.png",
+    ]);
+    let body = Matter.Bodies.circle(x, y, GameObjects.BLOCK_SIZE / 2, {
+      render: {
+        sprite: {
+          texture: txt,
+          xScale: GameObjects.BLOCK_SIZE / 64,
+          yScale: GameObjects.BLOCK_SIZE / 64,
+        },
+      },
+      restitution: 0.8,
+      collisionFilter: GameObjects._PRE_SHOT_COLLISION_FILTER,
+      label: "Projectile",
+      powerActivated: false,
+      launched: false,
+      activatePower: undefined,
+      frictionAir: 0.005,
+      sleepThreshold: 1500 / 16.666,
+      density: 0.007,
+    });
 
-        return body
+    return body;
+  }
+
+  static rect(x, y, width, height, material) {
+    let hp;
+    switch (material) {
+      case "glass":
+        hp = 2000;
+        break;
+      case "wood":
+        hp = 5000;
+        break;
+      case "stone":
+        hp = 7000;
+        break;
+      case "metal":
+        hp = 9000;
+        break;
+      default:
+        throw "Invalid block material";
     }
-
-    static rect(x, y, width, height, material) {
-        let hp;
-        switch (material){
-            case ('glass'):
-                hp = 2000;
-                break;
-            case ('wood'):
-                hp = 5000;
-                break;
-            case ('stone'):
-                hp = 7000;
-                break;
-            case ('metal'):
-                hp = 9000;
-                break;
-            default:
-                throw "Invalid block material"
+    let comp = Matter.Composites.stack(
+      0,
+      0,
+      width,
+      height,
+      0,
+      0,
+      (x, y, cols, rows) => {
+        let texture = "";
+        if (rows == 0) {
+          texture += "t";
         }
-        let comp = Matter.Composites.stack(0, 0, width, height, 0, 0, (x, y, cols, rows) => {
-            let texture = ""
-            if (rows == 0) {
-                texture += "t";
-            }
-            if (rows == height - 1) {
-                texture += "b";
-            }
-            if (cols == 0) {
-                texture += "l"
-            }
-            if (cols == width - 1) {
-                texture += "r"
-            }
-            let render;
-            if (texture == "") {
-                render = { visible: false }
-            } else {
-                render = { sprite: { texture: `images/Material Texture/${material}/${texture}.png`, xScale: GameObjects.BLOCK_SIZE / 64, yScale: GameObjects.BLOCK_SIZE / 64 } }
-                if(material == "glass"){
-                    render.opacity = 0.75
-                }
-            }
-            return Matter.Bodies.rectangle(x, y, GameObjects.BLOCK_SIZE, GameObjects.BLOCK_SIZE, { render: render});
-        })
-        let body = Matter.Body.create({ label: "Block", hp: hp, parts: comp.bodies, shockAbsorbed: 0, sleepThreshold: 1, frictionAir: 0.9, mass: 1})
-        Matter.Body.setPosition(body, { x: x, y: y })
-        Matter.Events.on(body,"sleepStart", (event)=>{
-            Matter.Body.setDensity(event.source,0.001)
-            event.source.frictionAir = 0.01
-        })
-        Matter.Events.on(body, "sleepEnd", (event) => {
-            event.source.sleepThreshold = Infinity
-        })
-        return body;
-    }
+        if (rows == height - 1) {
+          texture += "b";
+        }
+        if (cols == 0) {
+          texture += "l";
+        }
+        if (cols == width - 1) {
+          texture += "r";
+        }
+        let render;
+        if (texture == "") {
+          render = { visible: false };
+        } else {
+          render = {
+            sprite: {
+              texture: `images/Material Texture/${material}/${texture}.png`,
+              xScale: GameObjects.BLOCK_SIZE / 64,
+              yScale: GameObjects.BLOCK_SIZE / 64,
+            },
+          };
+          if (material == "glass") {
+            render.opacity = 0.75;
+          }
+        }
+        return Matter.Bodies.rectangle(
+          x,
+          y,
+          GameObjects.BLOCK_SIZE,
+          GameObjects.BLOCK_SIZE,
+          { render: render }
+        );
+      }
+    );
+    let body = Matter.Body.create({
+      label: "Block",
+      hp: hp,
+      parts: comp.bodies,
+      shockAbsorbed: 0,
+      sleepThreshold: 1,
+      frictionAir: 0.9,
+      mass: 1,
+    });
+    Matter.Body.setPosition(body, { x: x, y: y });
+    Matter.Events.on(body, "sleepStart", (event) => {
+      Matter.Body.setDensity(event.source, 0.001);
+      event.source.frictionAir = 0.01;
+    });
+    Matter.Events.on(body, "sleepEnd", (event) => {
+      event.source.sleepThreshold = Infinity;
+    });
+    return body;
+  }
 
-    static mouseConstraint(game) {
-        let gameMouse = Matter.Mouse.create(game.renderer.canvas,);
-        let mouseConstraint = Matter.MouseConstraint.create(game.engine, {
-            mouse: gameMouse,
-            collisionFilter: GameObjects._PRE_SHOT_COLLISION_FILTER,
-            constraint: {
-                stiffness: 1,
-                render: {
-                    visible: false
-                }
-            }
-        });
-        let scale = { x: Generator.WIDTH_RATIO * Generator.RENDER_SCALE / game.renderer.canvas.width, y: Generator.HEIGHT_RATIO * Generator.RENDER_SCALE / game.renderer.canvas.height }
-        Matter.Mouse.setScale(mouseConstraint.mouse, scale)
-        return mouseConstraint;
-    }
+  static mouseConstraint(game) {
+    let gameMouse = Matter.Mouse.create(game.renderer.canvas);
+    let mouseConstraint = Matter.MouseConstraint.create(game.engine, {
+      mouse: gameMouse,
+      collisionFilter: GameObjects._PRE_SHOT_COLLISION_FILTER,
+      constraint: {
+        stiffness: 1,
+        render: {
+          visible: false,
+        },
+      },
+    });
+    let scale = {
+      x:
+        (Generator.WIDTH_RATIO * Generator.RENDER_SCALE) /
+        game.renderer.canvas.width,
+      y:
+        (Generator.HEIGHT_RATIO * Generator.RENDER_SCALE) /
+        game.renderer.canvas.height,
+    };
+    Matter.Mouse.setScale(mouseConstraint.mouse, scale);
+    return mouseConstraint;
+  }
 
-    static slingShot(game, x, y) {
-        let constraint = Matter.Constraint.create({
-            label: "Sling Shot",
-            pointA: { x: x, y: y },
-            bodyB: GameObjects.projectile(x, y),
-            length: 0,
-            stiffness: 0.005,
-            render: {
-                type: "line",
-                anchors: false,
-            }
-        })
-        Matter.Events.on(game.mouseConstraint, "mouseup", () => {
-            let dist = Matter.Vector.magnitude(Matter.Vector.sub(constraint.bodyB.position, constraint.pointA))
-            if (dist > constraint.bodyB.circleRadius) {
-                constraint.bodyB.launched = true;
-                Matter.Body.setVelocity(constraint.bodyB, { x: 0, y: 0 })
-            }
-        })
-        Matter.Events.on(game.engine, "afterUpdate", () => {
-            //limit length
-            let diff = Matter.Vector.sub(constraint.bodyB.position, constraint.pointA);
-            let mag = Matter.Vector.magnitude(diff);
-            if (mag > GameObjects.SLING_SHOT_LENGTH) {
-                let offset = Matter.Vector.mult(diff, 1 - GameObjects.SLING_SHOT_LENGTH / mag);
-                let newPos = Matter.Vector.sub(constraint.bodyB.position, offset);
-                Matter.Body.setPosition(constraint.bodyB, newPos);
-                Matter.Body.setAngularVelocity(constraint.bodyB, 0)
-            }
-            //check for launch
-            if (constraint.bodyB.launched) {
-                let dist = Matter.Vector.magnitude(Matter.Vector.sub(constraint.bodyB.position, constraint.pointA))
-                let prevDist = Matter.Vector.magnitude(Matter.Vector.sub(constraint.bodyB.positionPrev, constraint.pointA))
-                if (dist < constraint.bodyB.circleRadius && prevDist > constraint.bodyB.circleRadius) {
-                    constraint.bodyB.collisionFilter = GameObjects._POST_SHOT_COLLISION_FILTER;
-                    constraint.bodyB = GameObjects.projectile(constraint.pointA.x, constraint.pointA.y)
-                }
-            }
-
-        })
-        return constraint
-    }
-
-
+  static slingShot(game, x, y) {
+    let constraint = Matter.Constraint.create({
+      label: "Sling Shot",
+      pointA: { x: x, y: y },
+      bodyB: GameObjects.projectile(x, y),
+      length: 0,
+      stiffness: 0.005,
+      render: {
+        type: "line",
+        anchors: false,
+      },
+    });
+    Matter.Events.on(game.mouseConstraint, "mouseup", () => {
+      let dist = Matter.Vector.magnitude(
+        Matter.Vector.sub(constraint.bodyB.position, constraint.pointA)
+      );
+      if (dist > constraint.bodyB.circleRadius) {
+        constraint.bodyB.launched = true;
+        Matter.Body.setVelocity(constraint.bodyB, { x: 0, y: 0 });
+      }
+    });
+    Matter.Events.on(game.engine, "afterUpdate", () => {
+      //limit length
+      let diff = Matter.Vector.sub(
+        constraint.bodyB.position,
+        constraint.pointA
+      );
+      let mag = Matter.Vector.magnitude(diff);
+      if (mag > GameObjects.SLING_SHOT_LENGTH) {
+        let offset = Matter.Vector.mult(
+          diff,
+          1 - GameObjects.SLING_SHOT_LENGTH / mag
+        );
+        let newPos = Matter.Vector.sub(constraint.bodyB.position, offset);
+        Matter.Body.setPosition(constraint.bodyB, newPos);
+        Matter.Body.setAngularVelocity(constraint.bodyB, 0);
+      }
+      //check for launch
+      if (constraint.bodyB.launched) {
+        let dist = Matter.Vector.magnitude(
+          Matter.Vector.sub(constraint.bodyB.position, constraint.pointA)
+        );
+        let prevDist = Matter.Vector.magnitude(
+          Matter.Vector.sub(constraint.bodyB.positionPrev, constraint.pointA)
+        );
+        if (
+          dist < constraint.bodyB.circleRadius &&
+          prevDist > constraint.bodyB.circleRadius
+        ) {
+          constraint.bodyB.collisionFilter =
+            GameObjects._POST_SHOT_COLLISION_FILTER;
+          constraint.bodyB = GameObjects.projectile(
+            constraint.pointA.x,
+            constraint.pointA.y
+          );
+        }
+      }
+    });
+    return constraint;
+  }
 }
