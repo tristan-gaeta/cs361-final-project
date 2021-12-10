17ad2a2ff82d84a53371d1ecde2878b776feec90
ClarkUPS <cbrace@pugetsound.edu>
2021-12-09 18:02:26 -0800

    fixed audio button and symbols thanks to skyler and added a few more pixel art elements

--- mainPage.js
+++ mainPage.js
@@ -1,15 +1,16 @@
 
 var audio = new Audio("sounds/Cloud Armada-TestSong.mp3");
-var counter = 0;
-//var button = document.getElementById("pop")
+var counter = true;
+var button = document.querySelector("#musicButton");
+console.log(button);
 function playMusic() {
-    //console.log(button);
-    if (counter % 2 == 0) {
+    if (counter == true) {
         audio.play()
-        //button.innerHTML() = "⏸"
+        button.innerHTML = "||"
+        counter = false;
     } else {
         audio.pause()
-       // button.innerHTML() = "▶"
+        button.innerHTML = "▶"
+        counter = true
     }
-    counter++
 }
