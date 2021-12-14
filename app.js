69e175dde055252f0d094775d23f9a00ee385b1a
Tristan Gaeta <tristangaeta@gmail.com>
2021-12-13 18:37:40 -0800

    SPACE

--- app.js
+++ app.js
@@ -15,6 +15,22 @@ document.getElementById("ballButton").addEventListener("click", (event) => {
         }
     }
 })
+document.addEventListener("keypress", (event) => {
+    if (event.key == " " && !game.slingShot.bodyB.velocityPrev) {
+        let equation = Equations.createProblem(game.difficulty);
+        let ans = window.prompt(`${equation.string.substring(1, equation.string.length - 1)} = `);
+        if (ans == equation.value) {
+            Matter.Composite.add(game.engine.world, game.slingShot.bodyB);
+            game.streaks++;
+            game.difficulty += Math.round(game.streaks / 5);
+        } else {
+            let diff = 1 - 1 / (game.streaks + 2);
+            game.difficulty *= diff;
+            game.difficulty = Math.floor(game.difficulty)
+            game.streaks = 0;
+        }
+    }
+})
 var audio = new Audio("sounds/Cloud Armada-TestSong.mp3");
 audio.loop = true;
 var counter = true;
